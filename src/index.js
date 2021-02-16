8aa89fb56d4dba44c03350d5701fe4ec059a72bb
sbmj1328 <78645924+sbmj1328@users.noreply.github.com>
2021-02-16 17:47:33 +0530

    local storage added for redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import {Provider} from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from 'redux-persist'
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store} >
-    <App />
+    <Provider store={store}  >
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/root_reducers.js
+++ src/redux/root_reducers.js
@@ -1,10 +1,20 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import todo from './slice/todoSlice';
-
+import todo from "./slice/todoSlice";
+import { persistCombineReducers, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const reducer = combineReducers({
-   ...todo
-})
+  todo: todo.todo,
+  selectedTodo: todo.selectedTodo,
+});
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist:["selectedTodo"],
+  blacklist:[],
+};
 
-export default reducer;
+const myPersistReducer = persistReducer(persistConfig, reducer)
+
+export default myPersistReducer;
